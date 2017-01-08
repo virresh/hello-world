d0219458dbfaffff46a661663148c560a9b771c2
Viresh Gupta <viresh16118@iiitd.ac.in>
2017-01-08 23:17:43 +0530

    Small to-do list

--- scripts/script1.js
+++ scripts/script1.js
@@ -1,6 +1,31 @@
 new Vue({
-  el: '#app',
-  data: {
-    message: 'based on BULMA and Vue.'
-  }
-})
+	el: '#events',					//targeted element
+	data: {
+		event: { 
+			title : '',	description : '' 
+		},	
+		events: []	
+	},	
+
+	ready: function() {
+		this.fetchEvents();
+	},
+
+	methods: {																//every other method
+		fetchEvents: function() {
+			var events = [];
+			this.$set('events',events);
+		},
+
+		addEvent: function() {
+			if(this.event.title){
+				this.events.push(this.event);
+				this.event = {title: '', description: ''};
+			}
+		},
+
+		cancel: function() {
+			this.event = {title: '', description: ''};
+		}
+	}
+});
